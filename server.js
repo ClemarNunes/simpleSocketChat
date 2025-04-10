d6d474481b72e163d31a3f74695092783d36ddc7
ClemarNunes <clemar_nunes@hotmail.com>
2025-04-10 16:15:33 +0100

    sending project

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io")
+// import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+// const io = new Server(server);
+
+server.listen(3000);
+
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let connectedUsers = [];
+
+io.on("connection", (socket) => {
+    console.log("conexão")
+
+    socket.on("join-request", (username) => {
+        socket.username = username;
+        connectedUsers.push(username);
+        console.log( connectedUsers );
+
+        socket.emit("user-ok", connectedUsers);
+        socket.broadcast.emit("list-update", {
+            joined: username,
+            list: connectedUsers
+        })
+    });
+
+
+    socket.on("disconnect", () => {
+        connectedUsers = connectedUsers.filter( u => u != socket.username);
+
+        socket.broadcast.emit("list-update", {
+            left: socket.username,
+            list: connectedUsers
+        });
+    }); 
+
+
+    socket.on("send-msg", (txt) => {
+        let obj = {
+            username: socket.username,
+            message: txt
+        };
+
+        // socket.emit("show-msg", obj);
+        socket.broadcast.emit("show-msg", obj);
+    })
+}); 
+
+ 
